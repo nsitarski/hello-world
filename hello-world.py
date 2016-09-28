5f02a5192fa451e07a31777cec7f85aa4d0f2fc9
nsitarski <nsitarski@gmail.com>
2016-09-27 21:19:21 -0400

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+#This is my first project from the book I bought "Python Crash Course"
+
+message = ("Hello Python World")
+print(message)
+
+message = ("hello Python Crash Course World!")
+print(message)
